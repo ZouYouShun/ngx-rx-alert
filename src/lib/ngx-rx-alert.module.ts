54105239bcf4e6f536bed392c8fe00a9e1cc50e0
ZouYouShun <itisalanlife@gmail.com>
2020-02-28 23:46:09 +0800

    update @angular/material

--- src/lib/ngx-rx-alert.module.ts
+++ src/lib/ngx-rx-alert.module.ts
@@ -2,7 +2,9 @@ import { A11yModule } from '@angular/cdk/a11y';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule, MatIconModule, MatInputModule } from '@angular/material';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 import { NgxRxAlertComponent } from './ngx-rx-alert.component';
 
